1b7d71ccde1141f027264944e4788f4b919e5883
25ag1a6613 <a5ag1a6613@gmail.com>
2026-08-20 09:38:17 +0000

    update

---
+++ ExampleNarrowingTypeCasting.java
@@ -0,0 +1,9 @@
+public class ExampleNarrowingTypeCasting {
+    public static void main(String args[])
+{
+double d = 166.66; int i = (int)d;
+System.out.println("Before conversion: "+d);
+System.out.println("After conversion into int type: "+i);
+}
+
+}
